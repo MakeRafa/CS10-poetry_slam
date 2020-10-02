bdddb2e4e2b85ce7396f7a6556ebfa15fa4431cf
Rafa <rafavazquez229@gmail.com>
2020-10-01 23:43:51 -0700

    print rnadom order lines of poem

--- main.py
+++ main.py
@@ -11,4 +11,20 @@ def lines_printed_backwards(lines_list):
     for line in lines_list:
         print(line)
 
+print("*************************************************************************")
+
 lines_printed_backwards(get_file_lines(filename))
+
+print("*************************************************************************")
+
+import random
+def lines_printed_random(lines_list):
+    random.shuffle(lines_list)
+    for line in lines_list:
+        print(line)
+
+lines_printed_random(get_file_lines(filename))
+
+print("*************************************************************************")
+
+# def lines_printed_custom():
