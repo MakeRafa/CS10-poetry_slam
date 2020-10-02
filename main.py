523a30f81c2c9c3c12ab15558f805fb5b7d883c2
Rafa <rafavazquez229@gmail.com>
2020-10-01 23:06:39 -0700

    Print poem backwords

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
 
 # This is a new python file
+filename = "poem.txt"
+
+def get_file_lines(filename):
+    read_poem = open(filename, 'r')
+    return read_poem.readlines()
+
+def lines_printed_backwards(lines_list):
+    lines_list = lines_list[::-1]
+    for line in lines_list:
+        print(line)
+
+lines_printed_backwards(get_file_lines(filename))
